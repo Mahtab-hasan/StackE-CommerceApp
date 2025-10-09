e7c84117e719fa3cc6756b7bdcd1939c18ad1fea
mahtab_hasan <124357232+Mahtab-hasan@users.noreply.github.com>
2025-10-09 17:34:01 +0600

    save

--- app/api/order/list/route.js
+++ app/api/order/list/route.js
@@ -19,16 +19,8 @@ export async function GET(request) {
         const orders = await Order.find({ userId }).populate('address items.product')        
 
         return NextResponse.json({ success: true, orders });
-        
+
     } catch (error) {
         return NextResponse.json({ success: false, message: error.message }, { status: 500 });
     }
 }
-
-
-
-// 3.30.59 time on youtube video orders part 
-// 3.30.59 time on youtube video orders part 
-// 3.30.59 time on youtube video orders part 
-// 3.30.59 time on youtube video orders part 
-// 3.30.59 time on youtube video orders part 
